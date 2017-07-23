42b1f1e98b13d6f7139c981b3ed34426916141f7
Gurcanakyuz <gurcan@pembepanjur.com>
2017-07-23 14:36:47 +0300

    Embed edilen videonun yüksekliği dinamikleştirildi.

--- Classes/VideoEmbedAssembler/videoEmbedAssembler.php
+++ Classes/VideoEmbedAssembler/videoEmbedAssembler.php
@@ -25,15 +25,15 @@ class videoEmbedAssembler
      * @param string $string
      * @return string
      */
-    public static function convertText(string $string):string
+    public static function convertText(string $string, int $embedHeight = 0):string
     {
 
         $string = urldecode($string);
 
         $output = linkAssembler::link($string);
-        $output = (self::$isAddedEmbed == false) ? self::youtubeEmbed($output) : $output;
-        $output = (self::$isAddedEmbed == false) ? self::vimeoEmbed($output) : $output;
-        $output = (self::$isAddedEmbed == false) ? self::dailymotionEmbed($output) : $output;
+        $output = (self::$isAddedEmbed == false) ? self::youtubeEmbed($output, $embedHeight) : $output;
+        $output = (self::$isAddedEmbed == false) ? self::vimeoEmbed($output, $embedHeight) : $output;
+        $output = (self::$isAddedEmbed == false) ? self::dailymotionEmbed($output, $embedHeight) : $output;
 
         return $output;
 
@@ -47,7 +47,7 @@ class videoEmbedAssembler
      * @param string $string
      * @return string
      */
-    protected static function youtubeEmbed(string $string):string
+    protected static function youtubeEmbed(string $string, int $embedHeight):string
     {
 
         if(stripos($string, "youtube.com") !== false || stripos($string, "youtu.be") !== false) {
@@ -57,7 +57,8 @@ class videoEmbedAssembler
                 preg_match(self::$regexYoutubeShort, $string, $video_id);
             }
             if(count($video_id) > 1 && strlen($video_id[1]) > 0) {
-                $string .= "<iframe width=\"100%\" height=\"".self::$embedHeight."\" src=\"https://www.youtube.com/embed/{$video_id[1]}\" frameborder=\"0\" allowfullscreen></iframe>";
+                $embedHeight = ($embedHeight > 0) ? $embedHeight : self::$embedHeight;
+                $string .= "<iframe width=\"100%\" height=\"".$embedHeight."\" src=\"https://www.youtube.com/embed/{$video_id[1]}\" frameborder=\"0\" allowfullscreen></iframe>";
                 self::$isAddedEmbed = true;
             }
 
@@ -73,7 +74,7 @@ class videoEmbedAssembler
      * @param string $string
      * @return string
      */
-    protected static function vimeoEmbed(string $string):string
+    protected static function vimeoEmbed(string $string, int $embedHeight):string
     {
 
         if(stripos($string, "vimeo.com") !== false) {
@@ -81,7 +82,8 @@ class videoEmbedAssembler
             preg_match(self::$regexVimeo, $string, $video_id);
 
             if(count($video_id) > 1 && strlen($video_id[1]) > 0) {
-                $string .= "<iframe src=\"https://player.vimeo.com/video/{$video_id[1]}\" width=\"100%\" height=\"".self::$embedHeight."\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
+                $embedHeight = ($embedHeight > 0) ? $embedHeight : self::$embedHeight;
+                $string .= "<iframe src=\"https://player.vimeo.com/video/{$video_id[1]}\" width=\"100%\" height=\"".$embedHeight."\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
                 self::$isAddedEmbed = true;
             }
 
@@ -98,7 +100,7 @@ class videoEmbedAssembler
      * @param string $string
      * @return string
      */
-    protected static function dailymotionEmbed(string $string):string
+    protected static function dailymotionEmbed(string $string, int $embedHeight):string
     {
 
         if(stripos($string, "dailymotion.com") !== false) {
@@ -108,7 +110,8 @@ class videoEmbedAssembler
                 preg_match(self::$regexDailymotionShort, $string, $video_id);
             }
             if(count($video_id) > 1 && strlen($video_id[1]) > 0) {
-                $string .= "<iframe frameborder=\"0\" width=\"100%\" height=\"".self::$embedHeight."\" src=\"//www.dailymotion.com/embed/video/{$video_id[1]}\" allowfullscreen></iframe>";
+                $embedHeight = ($embedHeight > 0) ? $embedHeight : self::$embedHeight;
+                $string .= "<iframe frameborder=\"0\" width=\"100%\" height=\"".$embedHeight."\" src=\"//www.dailymotion.com/embed/video/{$video_id[1]}\" allowfullscreen></iframe>";
                 self::$isAddedEmbed = true;
             }
 
--- test.php
+++ test.php
@@ -13,5 +13,5 @@ require 'vendor/autoload.php';
 
 use \IGA\VideoEmbedAssembler\videoEmbedAssembler;
 
-echo videoEmbedAssembler::convertText("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium, amet blanditiis dolore ea, eum eveniet, excepturi explicabo harum iure odio optio reprehenderit suscipit. Accusantium aliquam https://www.youtube.com/watch?v=8d97CGyglS4 facilis fugiat illo laboriosam libero nihil numquam optio perspiciatis quae. Ab asperiores at atque cupiditate eaque, https://vimeo.com/226441556 fugiat labore neque non pariatur quae quis vitae!");
+echo videoEmbedAssembler::convertText("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium, amet blanditiis dolore ea, eum eveniet, excepturi explicabo harum iure odio optio reprehenderit suscipit. Accusantium aliquam https://www.youtube.com/watch?v=8d97CGyglS4 facilis fugiat illo laboriosam libero nihil numquam optio perspiciatis quae. Ab asperiores at atque cupiditate eaque, https://vimeo.com/226441556 fugiat labore neque non pariatur quae quis vitae!", 100);
 
